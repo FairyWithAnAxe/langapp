6f5d5f0176eb6dfa9cbb56badaa78f64d868c69f
FairyWithAnAxe <79852767545@ya.ru>
2021-09-05 15:29:21 +0300

    +

--- src/components/Homework/TableRow.jsx
+++ src/components/Homework/TableRow.jsx
@@ -4,104 +4,144 @@ import TableButton from './TableButton';
 
 
 
-// function TableRow({ word }) {
-
-//     const [isSelected, toggleSelected] = useState(false)
-//     const [value, setValue] = useState({word})
-//     const { id, english, transcription, russian, tags } = word
-
-//     const handleChange = () => {
-//         toggleSelected(!isSelected);
-//     }
-
-//     return (
-
-//         {
-//             isSelected ?
-//                 ( <tr>
-//                     <tr>
-//                     <td>{word.id}</td>
-//                     <td><input onBlur={handleChange} onChange={(e) => setValue(e.target.value)} value={value} /></td>
-//                     <td><input onBlur={handleChange} onChange={(e) => setValue(e.target.value)} value={value} /></td>
-//                     <td><input onBlur={handleChange} onChange={(e) => setValue(e.target.value)} value={value} /></td>
-//                     <td><input onBlur={handleChange} onChange={(e) => setValue(e.target.value)} value={value} /></td>
-//                     <td>
-//                         <TableButton button="Save" onClick={handleChange} />
-//                         <TableButton button="Cancel" onClick={() => { handleChange(); setValue(word.english); }} />
-//                     </td>
-//                 </tr>
-//                 </tr > ) : (<tr>
-//                     <td>{word.id}</td>
-//                     <td>{word.english}</td>
-//                     <td>{word.transcription}</td>
-//                     <td>{word.russian}</td>
-//                     <td>{word.tags}</td>
-//                     <td>
-//                         <TableButton button="Edit" onClick={handleChange} />
-//                         <TableButton button="Del" />
-//                     </td>
-//                 </tr>)
-//         }
-//             )
-// }
-
-// export default TableRow
-
-
-
-
 function TableRow({ word }) {
 
-    const [isSelected, toggleSelected] = useState(false)
-    const [value, setValue] = useState(word.english)
     const { id, english, transcription, russian, tags } = word
+
+    const [isSelected, toggleSelected] = useState(false)
+
+    const [newWord, setNewWord] = useState({
+        id: id,
+        english: english,
+        transcription: transcription,
+        russian: russian,
+        tags: tags
+    })
+
 
     const handleChange = () => {
         toggleSelected(!isSelected);
     }
 
     return (
-        <tr>
-            <td>{word.id}</td>
-
-            {isSelected
-                ? <td><input
-                    onBlur={handleChange}
-                    onChange={(e) => setValue(e.target.value)}
-                    value={value} /></td>
-                : <td>{value}</td>
+        <>
+            {
+                isSelected
+                    ? (
+                        <tr>
+                            <td>{newWord.id}</td>
+                            <td>
+                                <input type="text"
+                                    value={newWord.english}
+                                    onChange={e => setNewWord({ ...newWord, english: e.target.value })}
+                                />
+                            </td>
+                            <td>
+                                <input type="text"
+                                    value={newWord.transcription}
+                                    onChange={e => setNewWord({ ...newWord, transcription: e.target.value })}
+                                />
+                            </td>
+                            <td>
+                                <input type="text"
+                                    value={newWord.russian}
+                                    onChange={e => setNewWord({ ...newWord, russian: e.target.value })}
+                                />
+                            </td>
+                            <td>
+                                <input type="text"
+                                    value={newWord.tags}
+                                    onChange={e => setNewWord({ ...newWord, tags: e.target.value })}
+                                />
+                            </td>
+                            <td>
+                                <TableButton button="Save" onClick={handleChange} />
+                                <TableButton button="Cancel" onClick={() => { handleChange(); setNewWord({ ...word }); }} />
+                            </td>
+                        </tr>
+                    )
+                    : (
+                        <tr>
+                            <td>{newWord.id}</td>
+                            <td>{newWord.english}</td>
+                            <td>{newWord.transcription}</td>
+                            <td>{newWord.russian}</td>
+                            <td>{newWord.tags}</td>
+                            <td>
+                                <TableButton button="Edit" onClick={handleChange} />
+                                <TableButton button="Del" />
+                            </td>
+                        </tr>
+                    )
             }
+        </>
 
 
-            {word.transcription && <td>{word.transcription}</td>
-            } {!word.transcription && <td>--</td>}
-
-
-            {word.russian && <td>{word.russian}</td>
-            } {!word.russian && <td>--</td>}
-
-
-            {word.tags && <td>{word.tags}</td>
-            } {!word.tags && <td>--</td>}
-
-
-            {isSelected
-                ? <td>
-                    <TableButton button="Save" onClick={handleChange} />
-                    <TableButton button="Cancel" onClick={() => { handleChange(); setValue(word.english); }} />
-                </td>
-                : <td>
-                    <TableButton button="Edit" onClick={handleChange} />
-                    <TableButton button="Del" />
-                </td>
-            }
-
-
-        </tr>
     )
 }
 
 export default TableRow
+
+
+
+
+    // function TableRow({ word }) {
+
+    //     const [isSelected, toggleSelected] = useState(false)
+    //     const [value, setValue] = useState(word.english)
+    //     const { id, english, transcription, russian, tags } = word
+
+    //     const handleChange = () => {
+    //         toggleSelected(!isSelected);
+    //     }
+
+    //     return (
+//     <tr tr >
+//     <td>{word.id}</td>
+
+// {
+//     isSelected
+//         ? <td><input
+//             onBlur={handleChange}
+//             onChange={(e) => setValue(e.target.value)}
+//             value={value} /></td>
+//         : <td>{value}</td>
+// }
+
+
+// {
+//     word.transcription && <td>{word.transcription}</td>
+// } { !word.transcription && <td>--</td> }
+
+
+// {
+//     word.russian && <td>{word.russian}</td>
+// } { !word.russian && <td>--</td> }
+
+
+// {
+//     word.tags && <td>{word.tags}</td>
+// } { !word.tags && <td>--</td> }
+
+
+// {
+//     isSelected
+//         ? <td>
+//             <TableButton button="Save" onClick={handleChange} />
+//             <TableButton button="Cancel" onClick={() => { handleChange(); setValue(word.english); }} />
+//         </td>
+//         : <td>
+//             <TableButton button="Edit" onClick={handleChange} />
+//             <TableButton button="Del" />
+//         </td>
+// }
+
+
+//         </tr >
+//     )
+// }
+
+// export default TableRow
 
 
 
